21e112988e321aa585a630f0378fe9f3ba42391a
mark-mcknight-oncorps <mark.mcknight@oncorps.io>
2025-02-08 13:21:14 -0500

    Broke up Experience into multiple sub-components for easier maintenance, updated cv.

---
+++ src/icons/icons.ts
@@ -0,0 +1,47 @@
+import AWS from "@/icons/AWS.astro";
+import Angular from "@/icons/Angular.astro";
+import CSS from "@/icons/css.astro";
+import Docker from "@/icons/Docker.astro";
+import Figma from "@/icons/Figma.astro";
+import Flutter from "@/icons/flutter.astro";
+import Git from "@/icons/git.astro";
+import GitHub from "@/icons/GitHub.astro";
+import Gitlab from "@/icons/Gitlab.astro";
+import HTML from "@/icons/html.astro";
+import JavaScript from "@/icons/javascript.astro";
+import Kotlin from "@/icons/kotlin.astro";
+import MySQL from "@/icons/sql.astro";
+import Next from "@/icons/next.astro";
+import Node from "@/icons/node.astro";
+import React from "@/icons/react.astro";
+import Swift from "@/icons/swift.astro";
+import SwiftUI from "@/icons/swiftui.astro";
+import Tailwind from "@/icons/tailwind.astro";
+import Terraform from "@/icons/Terraform.astro";
+import TypeScript from "@/icons/type.astro";
+import Vue from "@/icons/Vue.astro";
+
+export const ICONS: Record<string, any> = {
+  AWS,
+  Angular,
+  CSS,
+  Docker,
+  Figma,
+  Flutter,
+  Git,
+  GitHub,
+  Gitlab,
+  HTML,
+  JavaScript,
+  Kotlin,
+  MySQL,
+  Next,
+  Node,
+  React,
+  Swift,
+  SwiftUI,
+  Tailwind,
+  Terraform,
+  TypeScript,
+  Vue,
+};
